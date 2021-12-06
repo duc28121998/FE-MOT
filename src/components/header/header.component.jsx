eb8db525a0042bf0546d25b92b11fc5eae6e0f7f
unknown <duc25092000@gmail.com>
2021-12-06 18:09:46 +0700

    add

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
+
+import { auth } from '../../firebase/firebase.utils.js'
+// import { ReactComponent as Logo } from '../../assets/crown.svg'
+import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component.jsx'
+import { selectCartHidden } from '../../redux/cart/cart.selectors.js'
+import { selectCurrentUser } from '../../redux/user/user.selector.js'
+import { IconMenu, Logo, Ticker, Sneaker } from '../../assets/svgs/index'
+
+import './header.styles.scss'
+
+const Header = ({ currentUser, hidden }) => (
+  <div className="header row">
+    <Link className="logo-container" to="/">
+      <IconMenu />
+    </Link>
+
+    <div className="options">
+      <Logo />
+    </div>
+    {hidden ? null : <CartDropdown />}
+    <div className="king">
+      <div className="pr-4">
+        <Ticker />
+      </div>
+      <div>
+        <Sneaker /> <strong>0</strong>
+      </div>
+    </div>
+  </div>
+)
+
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  hidden: selectCartHidden,
+})
+
+export default connect(mapStateToProps)(Header)
